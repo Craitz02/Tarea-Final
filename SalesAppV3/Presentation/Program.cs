a4ea737d8646fd5b98d17f5c06de26b0b7c22e93
Craitz02 <rodrigocraitz02@gmail.com>
2021-06-21 09:08:17 -0600

    init

---
+++ SalesAppV3/Presentation/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Core;
+using Infraestructure.Data;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Presentation
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            IClientRepository clientRepository = new ClientRepository();
+            Application.Run(new Form1(clientRepository));
+        }
+
+        static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices((_, services) =>                    
+                   services.AddScoped<IClientRepository, ClientRepository>());
+    }
+}
